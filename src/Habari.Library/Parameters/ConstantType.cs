1f61b425552e10351251a3df9ace9b4d2631316f
Clément B <csluikidikilest@gmail.com>
2025-01-02 19:03:58 +0100

    Add Constant Type

---
+++ src/Habari.Library/Parameters/ConstantType.cs
@@ -0,0 +1,18 @@
+﻿namespace Habari.Library.Parameters
+{
+    public enum ConstantType
+    {
+        Text,
+        Integer,
+        Decimal,
+        Email,
+        Date,
+        Time,
+        DateTime,
+        Boolean,
+        List,
+        Phone,
+        Password,
+        Path,
+    }
+}
